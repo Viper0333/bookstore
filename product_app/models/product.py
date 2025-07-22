e874f2ba5c12ce3f7de5a3d40505cdf5082827d5
alexalexandre1989@hotmail.com <alexalexandre1989@hotmail.com>
2025-07-21 22:39:37 -0300

    Mensagem do commit

--- product_app/models/product.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from product_app.models import Category
-
-class Product(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField(max_length=500, blank=True, null=True)
-    price = models.PositiveBigIntegerField(null=True)
-    active = models.BooleanField(default=True)
-    category = models.ManyToManyField(Category, blank=True)
