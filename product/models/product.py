2d2a4f31bd671acf33be3423a677c415944a25a7
alexalexandre1989@hotmail.com <alexalexandre1989@hotmail.com>
2025-07-23 22:14:09 -0300

    realizado alterações de envio

--- product/models/product.py
+++ product/models/product.py
@@ -12,7 +12,8 @@ class Product(models.Model):
     description = models.TextField(max_length=500, blank=True, null=True)
     price = models.PositiveIntegerField(null=True)
     active = models.BooleanField(default=True)
-    category = models.ManyToManyField(Category, blank=True)
+    # category = models.ManyToManyField(Category, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', null=True, blank=True)
 
 
     def __str__(self):
