b689112922582fc17153b911e1c31a4080c7614a
alexalexandre1989@hotmail.com <alexalexandre1989@hotmail.com>
2025-08-31 11:20:27 -0300

    criando configuração de pagina

--- posts/urls.py
+++ posts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.feed, name='feed'),
+    path('', views.feed, name='post'),
     path('novo/', views.novo_post, name='novo_post'),
 ]
