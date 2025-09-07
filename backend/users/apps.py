9939e374b8dae0c2c4eedcebd22d8fca08169c04
alexalexandre1989@hotmail.com <alexalexandre1989@hotmail.com>
2025-09-06 22:51:21 -0300

    criado usario e senha

--- backend/users/apps.py
+++ backend/users/apps.py
@@ -1,9 +1,12 @@
 from django.apps import AppConfig
+import createsu
 
 class UsersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "users"
 
+    def ready(self):
+        createsu.setup_superuser_creation()
 
 
 
