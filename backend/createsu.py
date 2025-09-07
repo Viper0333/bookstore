cc04394320c48dcb2188239d1ea6682d71d9f9eb
alexalexandre1989@hotmail.com <alexalexandre1989@hotmail.com>
2025-09-06 22:56:20 -0300

    subido creatsu

--- backend/createsu.py
+++ backend/createsu.py
@@ -1,12 +1,18 @@
-from django.contrib.auth import get_user_model
 import os
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_migrate
 
-def create_superuser():
+def create_superuser_direct():
     User = get_user_model()
-    username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
-    email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
-    password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "admin123")
+    username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
+    email = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
+    password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "admin123")
 
     if not User.objects.filter(username=username).exists():
         User.objects.create_superuser(username=username, email=email, password=password)
-        print(f"Superuser {username} criado com sucesso!")
+        print(f"✅ Superusuário '{username}' criado.")
+    else:
+        print(f"ℹ️ Superusuário '{username}' já existe.")
+
+def setup_superuser_creation():
+    post_migrate.connect(lambda sender, **kwargs: create_superuser_direct())
