3525d153913472114221a9a8a8988df833c7f36d
alexalexandre1989@hotmail.com <alexalexandre1989@hotmail.com>
2025-09-06 21:46:53 -0300

    atualizado15

--- backend/twitter_clone/settings.py
+++ backend/twitter_clone/settings.py
@@ -23,8 +23,7 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "rest_framework_simplejwt",
     "corsheaders",
-    # "twitter_clone.apps.TwitterCloneConfig",
-    "users.apps.UsersConfig",
+    # "users.apps.UsersConfig",
 ]
 
 APPEND_SLASH = False
