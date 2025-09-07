1e6f67ffb1fc027d0297bee1c8e949b8a69663a5
alexalexandre1989@hotmail.com <alexalexandre1989@hotmail.com>
2025-09-06 22:28:42 -0300

    atualizado admin

--- backend/tweets/apps.py
+++ backend/tweets/apps.py
@@ -1,10 +1,6 @@
 from django.apps import AppConfig
-import createsu
 
 class TweetsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "tweets"
-
-    # def ready(self):
-    #     createsu.setup_superuser_creation()
 
--- backend/users/apps.py
+++ backend/users/apps.py
@@ -1,12 +1,9 @@
 from django.apps import AppConfig
-import createsu
 
 class UsersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "users"
 
-    # def ready(self):
-    #     createsu.setup_superuser_creation()
 
 
 
