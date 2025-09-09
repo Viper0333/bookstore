c8a3f0549d8951fd1156588a4ac890342d3133a6
alexalexandre1989@hotmail.com <alexalexandre1989@hotmail.com>
2025-09-09 14:13:58 -0300

    alterado settings

--- backend/twitter_clone/settings.py
+++ backend/twitter_clone/settings.py
@@ -33,6 +33,7 @@ APPEND_SLASH = False
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -41,8 +42,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
+
 
 ROOT_URLCONF = "twitter_clone.urls"
 
